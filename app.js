0d0363dda27a8f26532abf0cbace66dfb6a5abc7
cst7785 <cestevens95@gmail.com>
2022-04-07 14:44:29 -0400

    Experimented with animation of units accross the page by reading HTMLObject.offsetTop/offsetLeft and writing HTMLObject.styles.top. Additionally, began to implement classes for the View, Controller, and Data as well as classes for entities within the game such as units and towers.

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+class View {
+    constructor(){
+        this.activeUnits = [];
+    }
+}
+class Controller {
+    constructor() {
+
+    }
+}
+class Data {
+    constructor() {
+        this.playerUnits = [];
+        this.enemyUnits = [];
+    }
+}
+class Motion {
+    constructor() {
+        this.coords= []; //vector
+        this.speed; //magnitude
+        this.direction; //unit vector
+    }
+}
+class Unit {
+    constructor(name) {
+        this.name = name;
+        this.motion = new Motion();
+    }
+}
+class Tower {
+    constructor() {
+    }
+}
+
+
+
+
+
 //temporary functions for development
 function setTerrain() {
     const pathTiles = [4,5,14,15,24,25,34,35,44,45,54,55,64,65,74,75,84,85,94,95];
@@ -15,3 +53,40 @@ function setTerrain() {
     }
 }
 setTerrain();
+
+function moveDown() {
+    const unit1 = document.querySelector(".board-element");
+    currentPosition = unit1.offsetTop;
+    console.log(currentPosition);
+    speed = 1;
+    nextPosition = currentPosition + speed;
+    console.log(nextPosition)
+    unit1.style.top = `${nextPosition}px`;       
+}
+// const test = setInterval(moveDown, 50);
+
+//TODOs
+//pseudocoding
+function init(){};
+function render(){};
+// goal is to have a game loop on interval
+function gameLoop(){
+    //process user input
+    //update data model
+    //render updates
+};
+const loop = setInterval(gameLoop, 50);
+
+//create a wave class/object that creates a specified number of unit objects with the specified parameters
+//How to easily change properties of the unit such as health, armor, speed in between waves?
+
+const appView = new View();
+currentWave = [];
+let numEnemies = 5;
+for (let i = 0; i < numEnemies; i++) {
+    currentWave[i] = new Unit (`Unit ${i}`);
+    appView.activeUnits.push(currentWave[i].name);
+};
+console.log(currentWave);
+console.log(appView.activeUnits);
+
